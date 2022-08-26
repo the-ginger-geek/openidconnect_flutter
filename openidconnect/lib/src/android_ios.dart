475af94d34f57a9ee084d67fb09b0822dfb5de4f
neilhogg <6nDpEsWPqJegskZF7sG_>
2022-08-26 15:36:41 +0200

    Added the ability to hide the webview if prompt is set to none

--- openidconnect/lib/src/android_ios.dart
+++ openidconnect/lib/src/android_ios.dart
@@ -9,86 +9,97 @@ class OpenIdConnectAndroidiOS {
     Color? appBarBackgroundColor,
     Color? appBarForegroundColor,
     Function? cookiesCallback,
+    bool hideWebView = false,
   }) async {
     final cookieManager = CookieManager.WebviewCookieManager();
     final result = await showDialog<String>(
       context: context,
       useSafeArea: false,
+      barrierDismissible: true,
+      barrierColor: hideWebView ? Colors.transparent : Colors.black.withOpacity(0.3),
       builder: (dialogContext) {
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Material(
-              color: Colors.transparent,
-              child: Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.6,
-                        height: 65,
-                        child: Center(
-                          child: Text(
-                            authorizationUrl,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              color: appBarForegroundColor,
+        return Visibility(
+          visible: !hideWebView,
+          maintainState: true,
+          maintainSize: true,
+          maintainAnimation: true,
+          maintainInteractivity: true,
+          maintainSemantics: true,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Material(
+                color: Colors.transparent,
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.6,
+                          height: 65,
+                          child: Center(
+                            child: Text(
+                              authorizationUrl,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 14.0,
+                                color: appBarForegroundColor,
+                              ),
                             ),
                           ),
                         ),
                       ),
+                      IconButton(
+                        icon: Icon(Icons.close),
+                        color: appBarForegroundColor,
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: appBarBackgroundColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
                     ),
-                    IconButton(
-                      icon: Icon(Icons.close),
-                      color: appBarForegroundColor,
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ],
-                ),
-                decoration: BoxDecoration(
-                  color: appBarBackgroundColor,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
                   ),
                 ),
               ),
-            ),
-            Container(
-              color: Colors.white,
-              height: MediaQuery.of(context).size.height - (Platform.isIOS ? 130 : 105),
-              child: flutterWebView.WebView(
-                javascriptMode: flutterWebView.JavascriptMode.unrestricted,
-                initialUrl: authorizationUrl,
-                onPageFinished: (url) async {
-                  final cookies = await cookieManager.getCookies(url);
-                  if (cookies.isNotEmpty) {
-                    cookiesCallback?.call(cookies);
-                  }
-                  if (url.startsWith(redirectUrl)) {
-                    Navigator.of(context, rootNavigator: true).pop(url);
-                  }
-                },
-                navigationDelegate: (flutterWebView.NavigationRequest request) {
-                  // Because of a bug on the flutter webview we have to use the navigateionDelegate
-                  // callback to catch the redirect URL for iOS and the onPageFinished callback
-                  // for android.
-                  if (request.url.startsWith(redirectUrl) && Platform.isIOS) {
-                    Navigator.of(context, rootNavigator: true).pop(request.url);
-                    return flutterWebView.NavigationDecision.prevent;
-                  }
+              Container(
+                color: Colors.white,
+                height: MediaQuery.of(context).size.height - (Platform.isIOS ? 130 : 105),
+                child: flutterWebView.WebView(
+                  javascriptMode: flutterWebView.JavascriptMode.unrestricted,
+                  initialUrl: authorizationUrl,
+                  onPageFinished: (url) async {
+                    final cookies = await cookieManager.getCookies(url);
+                    if (cookies.isNotEmpty) {
+                      cookiesCallback?.call(cookies);
+                    }
+                    if (url.startsWith(redirectUrl)) {
+                      Navigator.of(context, rootNavigator: true).pop(url);
+                    }
+                  },
+                  navigationDelegate: (flutterWebView.NavigationRequest request) {
+                    // Because of a bug on the flutter webview we have to use the navigateionDelegate
+                    // callback to catch the redirect URL for iOS and the onPageFinished callback
+                    // for android.
+                    if (request.url.startsWith(redirectUrl) && Platform.isIOS) {
+                      Navigator.of(context, rootNavigator: true).pop(request.url);
+                      return flutterWebView.NavigationDecision.prevent;
+                    }
 
-                  return flutterWebView.NavigationDecision.navigate;
-                },
+                    return flutterWebView.NavigationDecision.navigate;
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
