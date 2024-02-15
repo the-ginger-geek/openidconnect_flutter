ee7b838a1f42c3e4e3913d853e3e728320eda2fd
Aymen Nurhussen <37356773+Eimen2018@users.noreply.github.com>
2024-02-15 10:28:24 +0000

    resolve conflict

--- openidconnect/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ openidconnect/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,9 +11,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-<<<<<<< HEAD
-=======
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
->>>>>>> upstream/main
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
